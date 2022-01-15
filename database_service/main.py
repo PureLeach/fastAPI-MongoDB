78608bb75b062ba5a2299cd117415541b5232e69
barannikovMU <barannikov.mu@gmail.com>
2022-01-15 23:32:35 +0300

    update main.py

--- database_service/main.py
+++ database_service/main.py
@@ -8,11 +8,6 @@ app = FastAPI()
 app.include_router(endpoints.router)
 
 
-@app.get("/")
-def index():
-    return "Hello world"
-
-
 if __name__ == '__main__':
     """Launched with `python main.py` at root level"""
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
