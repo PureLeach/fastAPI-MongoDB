b96838ddf9cd71487f59d1f49ec003cc15233e9a
barannikovMU <barannikov.mu@gmail.com>
2022-01-15 23:46:23 +0300

    added CRUD endpoints

--- database_service/api/endpoints.py
+++ database_service/api/endpoints.py
@@ -1,49 +1,51 @@
 from typing import List
+import shutil
+from datetime import datetime, time, timedelta
+import random
+from uuid import UUID
 
-from fastapi import APIRouter, Depends
-
-from .schemas import FileBase
-# from sqlalchemy.orm import Session
-# from db.database import get_db
-# from db import db_user
-
-
-router = APIRouter(
-  prefix='/file',
-  tags=['file']
+from fastapi import (
+    APIRouter,
+    Depends,
+    status,
+    Response,
+    Request,
+    HTTPException,
+    UploadFile,
+    File,
 )
 
-
-# Create file
-@router.post('/', response_model=FileBase)
-def create_file(request: FileBase):
-    return "Create file"
-#   return db_user.create_user(db, request)
+from .schemas import FileSchema
 
 
-# Read all files
-@router.get('/', response_model=List[FileBase])
+router = APIRouter(prefix="/file", tags=["file"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_file(request: FileSchema = Depends(), file: UploadFile = File(...)):
+    with open(f"data/{random.random()}.png", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {**request.dict(), "file_name": file.filename}
+
+
+@router.get("/")
 def get_all_files():
     return "Read all files"
-#   return db_user.get_all_users(db)
 
 
-# Read one file
-@router.get('/{id}', response_model=FileBase)
-def get_file(id: int):
-    return f"Read one file {id}"
-#   return db_user.get_user(db, id)
+@router.get("/{id}")
+def get_file(id: int, response: Response):
+    if id < 5:
+        return f"Read one file {id}"
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
 
 
-# Update file
-@router.put('/{id}')
-def update_file(id: int, request: FileBase):
-    return f"Update file {id}"
-#   return db_user.update_user(db, id, request)
+@router.put("/{id}", status_code=status.HTTP_200_OK)
+def update_file(id: int, request: FileSchema):
+    return request
 
 
-# Delete file
-@router.delete('/{id}')
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_file(id: int):
     return f"Delete file {id}"
-#   return db_user.delete_user(db, id)
