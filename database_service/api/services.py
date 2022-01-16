eb09a04d717ee834588bd1d671d7ab31308cfd5e
barannikovMU <barannikov.mu@gmail.com>
2022-01-16 16:09:07 +0300

    added func save_file

--- database_service/api/services.py
+++ database_service/api/services.py
@@ -1,13 +1,34 @@
-from typing import List
+from lib2to3.pgen2.token import OP
+import shutil
+from typing import List, Optional
+import pathlib
 from bson.objectid import ObjectId
-from uuid import UUID
+from uuid import UUID, uuid4
+import random
 import datetime
 
-from core.settings import collection
+from fastapi import UploadFile, HTTPException, status
+
+from core.settings import collection, logger
 
 
-def post_file(data: dict) -> dict:
-    pass
+def save_file(file: UploadFile) -> dict:
+    try:
+        file_uuid: str = str(uuid4())
+        file_extension: str = pathlib.Path(file.filename).suffix
+        file_path: str = f"data/{file_uuid + file_extension}"
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        file_info: dict = {"file_uuid": file_uuid, "file_name": file.filename, "file_path": file_path, "user_id": str(uuid4())}
+        return file_info
+    except Exception as e:
+        logger.error(f"Error saving the file to disk: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error saving the file to disk")
+    
+
+def post_file(file_data: dict) -> str:
+    file_id: ObjectId  = collection.insert_one(file_data).inserted_id
+    return str(file_id)
 
 
 def get_all_files() -> List[dict]:
