cb4191d1848530fcc8b090f17735c731f2a465a8
barannikovMU <barannikov.mu@gmail.com>
2022-01-16 03:46:27 +0300

    added services.py

---
+++ database_service/api/services.py
@@ -0,0 +1,26 @@
+from typing import List
+from bson.objectid import ObjectId
+from uuid import UUID
+import datetime
+
+from core.settings import collection
+
+
+def post_file(data: dict) -> dict:
+    pass
+
+
+def get_all_files() -> List[dict]:
+    pass
+
+
+def get_file(id: str) -> dict:
+    pass
+
+
+def update_file(id: str) -> dict:
+    pass
+
+
+def delete_file(id: str) -> bool:
+    pass
